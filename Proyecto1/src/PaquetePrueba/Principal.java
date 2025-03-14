8bb5a688cd8dcee725852e0fef6c35f2e521740c
Mjjunior57 <mjnn524@gmail.com>
2025-03-14 17:38:56 +0100

    Confirmacion de los cambios

---
+++ Proyecto1/src/PaquetePrueba/Principal.java
@@ -0,0 +1,10 @@
+package PaquetePrueba;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Proyecto1/src/PaquetePrueba/package-info.java
@@ -0,0 +1,1 @@
+package PaquetePrueba;
